da41d6a673b5e01a0c66414c7512707f06ea5a94
Tor Strimbold <torstri@kth.se>
2021-11-26 16:40:54 +0100

    Fixat setup för start

--- helloWorld/startGame.c
+++ helloWorld/startGame.c
@@ -1,27 +1,44 @@
 #include <pic32mx.h>
 #include <stdint.h>
 
-void draw_start(uint8_t *data){
-
-}
-
 void draw_arena(uint8_t *data){
 
-    // Left upper corner = (20, 5)
-    // Right upper corner = (108, 5)
-    // Left lower corner = (20, 27)
-    // Right lower corner = (108, 27)
+    // Roof left = (0, 0)
+    // Roof right = (127, 0)
+    // Floor left = (0, 31)
+    // Floor right = (127, 31)
 
     // Draw floor and roof
-    int x = 20;
-    for(x = 20; x <= 108; x++){
-        set_pixel(x, 5); // Draws roof
-        set_pixel(x,27); // Draws floor
+    int x;
+    for(x = 0; x < 128; x++){
+        set_pixel(x, 0); // Draws roof
+        set_pixel(x,31); // Draws floor
     }
     //Draw sides
-    int y = 5;
-    for(y = 5; y <= 27; y++){
-        set_pixel(108,y); // Right side
-        set_pixel(20, y); // Left side
+    int y;
+    for(y = 0; y < 32; y++){
+        set_pixel(0, y); // Left side
+        set_pixel(127,y); // Right side
     }
+}
+
+void draw_sliders(uint8_t *data){
+    // Left slider
+   setXPosLeft(16);
+   setYPosLeft(9);
+   setXPosRight(112);
+   setYPosRight(9);
+   drawSliders();
+}
+
+/**
+ * @brief Draws the starting point for the game
+ * arena and sliders
+ * 
+ * @param data 
+ */
+void draw_start(uint8_t *data){
+    draw_arena(data);
+    draw_sliders(data);
+
 }
