162c37fa571f2050580736ea041d69ab7231e103
Tor Strimbold <torstri@kth.se>
2021-11-26 16:41:27 +0100

    Fixat så sliders kan ritas och hållas koll på

--- helloWorld/sliders.c
+++ helloWorld/sliders.c
@@ -1,24 +1,59 @@
 #include <pic32mx.h>
 #include <stdint.h>
 
-int xPos;
-int yPos; // Middle of slider
+int xPosLeft; // Left point
+int yPosLeft; // Upper point of slider
+int xPosRight;
+int yPosRight;
+const int height = 13;
+const int width = 2;
 
-int getXPos(){
-    return xPos;
+int getXPosLeft(){
+    return xPosLeft;
 }
 
-void setXPos(int x){
-    xPos = x;
+int getXPosRight(){
+    return xPosRight;
 }
 
-int getYPos(){
-    return yPos;
+void setXPosLeft(int x){
+    xPosLeft = x;
 }
 
-void setYPos(int y){
-    yPos = y;
+void setXPosRight(int x){
+    xPosRight = x;
 }
 
+int getYPosLeft(){
+    return yPosLeft;
+}
+
+int getYPosRight(){
+    return yPosRight;
+}
+
+void setYPosLeft(int y){
+    yPosLeft = y;
+}
+
+void setYPosRight(int y){
+    yPosRight = y;
+}
+
+/**
+ * @brief Draws the sliders
+ * 
+ */
+void drawSliders(){
+    int yLeft = yPosLeft;
+    int yRight = yPosRight;
+    int i; // Length of slider
+    for(i = height; i > 0; i --){
+        set_pixel(xPosLeft,yLeft);
+        set_pixel(xPosRight,yRight);
+        yLeft ++;
+        yRight ++;
+    }
+}
 
 
