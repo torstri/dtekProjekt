e62accc43418a6cfa157b8af29f6095005cfb97d
Tor Strimbold <torstri@kth.se>
2021-11-28 16:06:09 +0100

    Försöker med timers

---
+++ helloWorld/timers.c
@@ -0,0 +1,8 @@
+#include <pic32mx.h>
+#include <stdint.h>
+
+void timerInit(){
+    T2CON = 0x0; // Reset clock incase
+    T2CON |= 0x800; // Set timer on
+    T2Con |= 0x70; // Set prescale to 256
+}
