7d9d849a34e4f216a520a4a04ee888cbc7f6277e
Tor Strimbold <torstri@kth.se>
2021-11-30 15:23:51 +0100

    Hade ett typo

--- helloWorld/timers.c
+++ helloWorld/timers.c
@@ -2,6 +2,12 @@
 #include <stdint.h>
 
 void timerInit(){
+
+    // Initialize interrupts and set priority
+    IPCSET(2) = 0x0000000C;
+    IPCSET(2) = 0x00000001;
+    IECSET(0) = 0x00000100;
+    
     T2CON = 0x0; // Reset clock incase
     T2CON |= 0x800; // Set timer on
     T2CON |= 0x70; // Set prescale to 256
